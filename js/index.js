3d8ab3f8f10c19ab5036e850b15a9523a8788ca6
pepesan <pepesan@gmail.com>
2020-01-31 09:49:03 +0100

    metida función que hace cosas como rajoy

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
+
+function escribe_cosas(){
+    document.write("Cosas");
+}
+
 function main(){
     /*
     función principal del frontend
      */
     console.log("Iniciando");
+    escribe_cosas();
 }
 document.addEventListener("DomContentLoaded", main);
